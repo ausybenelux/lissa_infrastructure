58aad842564cc52e8ad79feaaf33b03de30bd523
Klaas Van Waesberghe <klaasvw@gmail.com>
2015-02-09 09:30:11 +0100

    Update readme with deploy instructions.

--- chef-repo/site-cookbooks/lissa/recipes/authorized_keys.rb
+++
@@ -1,6 +0,0 @@
-file '/home/' + node['web_user'] + '/.ssh/authorized_keys' do
-  owner     #{web_user}
-  group     #{web_user}
-  mode      '0600'
-  content   node['lissa'].authorized_keys.join("\n")
-end
